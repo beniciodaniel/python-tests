fdeb9e7f74c79ac4ac6286f25ed41a978abc665a
Benicio <benicio.daniel@gmail.com>
2019-12-30 10:55:37 -0300

    Implements Leilao refactoring

--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from src.leilao.dominio import Usuario, Lance, Leilao
 
-class TestAvaliador(TestCase):
+class TestLeilao(TestCase):
 
     def setUp(self):
         self.beni = Usuario("Beni")
@@ -61,3 +61,31 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_deve_permitir_propor_lance_caso_o_leilao_nao_tenha_lances(self):
+        self.leilao.propoe(self.lance_do_beni)
+
+        quantidade_de_lances_recebida = len(self.leilao.lances)
+
+        self.assertEqual(1, quantidade_de_lances_recebida)
+
+    def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
+        yuri = Usuario("Yuri")
+        lance_do_yuri = Lance(yuri, 200)
+
+        self.leilao.propoe(self.lance_do_beni)
+        self.leilao.propoe(lance_do_yuri)
+
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+
+        self.assertEqual(2, quantidade_de_lances_recebido)
+
+    def test_nao_deve_permitir_propor_lance_caso_usuario_seja_o_mesmo(self):
+        lance_do_beni200 = Lance(self.beni, 200)
+
+        self.leilao.propoe(self.lance_do_beni)
+        self.leilao.propoe(lance_do_beni200)
+
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+
+        self.assertEqual(1, quantidade_de_lances_recebido)
