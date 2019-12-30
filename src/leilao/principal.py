603e920cf5b5279931fcae0b016190a1dffb3856
Benicio <benicio.daniel@gmail.com>
2019-12-29 21:49:51 -0300

    Refactors principal.py

--- src/leilao/principal.py
+++ src/leilao/principal.py
@@ -10,14 +10,13 @@ lance_do_daniel = Lance(daniel, 150.00)
 
 leilao = Leilao("Celular")
 
-leilao.lances.append(lance_do_bruno)
-leilao.lances.append(lance_do_beni)
-leilao.lances.append(lance_do_daniel)
+leilao.propoe(lance_do_bruno)
+leilao.propoe(lance_do_beni)
+leilao.propoe(lance_do_daniel)
 
 for lance in leilao.lances:
     print (f'O usuário {lance.usuario.nome} deu o lance de R$ {lance.valor}')
 
-avaliador = Avaliador()
-avaliador.avalia(leilao)
 
-print(f'O menor lance foi de R$ {avaliador.menor_lance} e o maior lance foi de R${avaliador.maior_lance}')
+
+print(f'O menor lance foi de R$ {leilao.menor_lance} e o maior lance foi de R${leilao.maior_lance}')
